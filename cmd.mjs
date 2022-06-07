e582ddbe69457b54b40075cc1fcd7a65f53c3b89
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-08 00:10:50 +0900

    return error the moment a command does so

--- cmd.mjs
+++ cmd.mjs
@@ -3,7 +3,7 @@ import { spawn } from "node:child_process";
 export function cmd(...command) {
   console.log(command);
   let p = spawn(command[0], command.slice(1));
-  return new Promise((resolveFunc) => {
+  return new Promise((resolveFunc, rejectFunc) => {
     p.stdout.on("data", (x) => {
       process.stdout.write(x.toString());
     });
@@ -11,7 +11,12 @@ export function cmd(...command) {
       process.stderr.write(x.toString());
     });
     p.on("exit", (code) => {
-      resolveFunc(code);
+      if (code === 0) {
+        resolveFunc(code);
+      } else {
+        console.error(`${command[0]} exited with ${code}`);
+        rejectFunc(code);
+      }
     });
   });
 }
